f98a1aaa91a835da6bf8f8341c0d28123b6b000f
Sergey <sergey@email.ru>
2021-09-17 10:23:28 +0300

    Routing

--- pub_sub/emit_log.php
+++ pub_sub/emit_log.php
@@ -8,17 +8,16 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $conn = new AMQPStreamConnection('rabbitmq', '5672', 'root', 'root');
 $channel = $conn->channel();
 
-$channel->exchange_declare('logs', 'fanout', false, false, false);
+$channel->exchange_declare('logs', 'direct', false, false, false);
 
-$messageBody = (string)$argv[1] ?? '';
-$msg = new AMQPMessage(
-    $messageBody,
-    ['delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT]
-);
+$messageSeverity  = $argv[1] ? strtolower($argv[1]) : 'info';
+$messageBody = (string)$argv[2] ?? '';
 
-$channel->basic_publish($msg, 'logs');
+$msg = new AMQPMessage($messageBody);
 
-echo "[x] Sent ", $messageBody, "\n";
+$channel->basic_publish($msg, 'logs', $messageSeverity);
+
+echo "[x] Sent ", $messageSeverity, ': ', $messageBody, "\n";
 
 $channel->close();
 $conn->close();
--- pub_sub/log_receiver.php
+++ pub_sub/log_receiver.php
@@ -7,12 +7,16 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $conn = new AMQPStreamConnection('rabbitmq', '5672', 'root', 'root');
 $channel = $conn->channel();
 
-$channel->exchange_declare('logs', 'fanout', false, false, false);
+$channel->exchange_declare('logs', 'direct', false, false, false);
 list($queueName, ,) = $channel->queue_declare('', false, false, true, false);
-$channel->queue_bind($queueName, 'logs');
+
+$severities = array_slice($argv, 1);
+foreach ($severities as $severity) {
+    $channel->queue_bind($queueName, 'logs', $severity);
+}
 
 $callback = function ($msg) {
-    echo "[x] First receiver got message: ", $msg->body, "\n";
+    echo "[x] ", $msg->delivery_info['routing_key'], ": ", $msg->body, "\n";
 };
 
 $channel->basic_consume($queueName, '', false, true, false, false, $callback);
